cdb2ae514e122c56af9cfd8bbbe8c32d0b86ae24
Dmitry Krylov <dmitry.krylov@gmail.com>
2019-03-28 23:23:59 +0400

    промежуточный коммит

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,11 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    '''Тест на токсичность'''
-
-    def  test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+    """Тест домашней страницы"""
+    
+    def test_root_url_resolves_to_home_page_view(self):
+        '''тест: корневой URL преобразуется в представление домашней страницы'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
