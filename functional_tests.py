2e224eb48f6e64a7a85ecae266318f985adced46
Dmitry Krylov <dmitry.krylov@gmail.com>
2019-04-02 00:09:09 +0400

    Базовое представление теперь возвращает минимальный HTML

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	'''Тест нового посетителя'''
 
-assert 'Django' in browser.title
+	def setUp(self):
+		'''установка (конструктор)'''
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		'''демонтаж (деструктор)'''
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		'''тест: можно начать список и получить его позже'''
+		# открываем домашнюю страницу
+		self.browser.get('http://localhost:8000')
+		# заголовок и шапка должны соответствовать
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Закончить тест!')
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home_page():
+def home_page(request):
     '''Домашняя страница'''
-    pass
+    return HttpResponse('<html><title>To-Do lists</title></html>')
