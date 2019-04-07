e7c6f6cc9243b603fe4f046e7e3fde6692932e45
Dmitry Krylov <dmitry.krylov@gmail.com>
2019-04-07 16:24:33 +0400

    ФТ проверяет возможность ввести новый эл-т

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
 	'''Тест нового посетителя'''
-
 	def setUp(self):
 		'''установка (конструктор)'''
 		self.browser = webdriver.Firefox()
@@ -18,6 +19,28 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 		# заголовок и шапка должны соответствовать
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+		
+		#Предлагается сразу ввести эл-и списка
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+		#Добавляем что нужно сделать
+		inputbox.send_keys('Купить павлиньи перья')
+
+		#после нажатия enter, страница обновляетсяи содержит эл-т списка
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Купить павлиньи перья' for row in rows)
+		)
+
 		self.fail('Закончить тест!')
 
 if __name__ == '__main__':
